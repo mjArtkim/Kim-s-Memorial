4ff5553b482b5763aadafd5b5eab921cb0378ee8
ART <mjtwins1@naver.com>
2026-01-30 15:48:01 +0900

    add lang and mainview

---
+++ frontend/src/components/MainView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LanguageSelect from '@/components/unit/LanguageSelect.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <LanguageSelect></LanguageSelect>
+  <div>{{ t('app.title') }}</div>
+</template>
